c64fb6b09eb14bc7a5515fcf18b2fbbd9c5c3d1f
zahid-bracu <zahid.mym@gmail.com>
2021-04-22 18:19:48 +0600

    ok

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const path=require('path');
 const app = express();
+require('dotenv').config()
 const port =process.env.PORT || 3200;
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: false })); //must import 
+app.use(express.static(path.join(__dirname,'public'))) 
 
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname,'public/index.html'))
-})
+ 
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
